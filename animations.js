74befca225d354262c14c3542df1cba1b47463b4
Zach Lefkovitz <zachlefko@gmail.com>
2020-03-24 11:28:10 -0500

    Sine wave animation on contact form

--- animations.js
+++ animations.js
@@ -1,6 +1,10 @@
-const primary = getComputedStyle(document.body).getPropertyValue('--primary');
-const textPrimary = getComputedStyle(document.body).getPropertyValue('--text-primary');
-const secondary = getComputedStyle(document.body).getPropertyValue('--secondary');
+const primary = getComputedStyle(document.body).getPropertyValue("--primary");
+const textPrimary = getComputedStyle(document.body).getPropertyValue(
+  "--text-primary"
+);
+const secondary = getComputedStyle(document.body).getPropertyValue(
+  "--secondary"
+);
 
 console.log(textPrimary);
 
@@ -14,7 +18,7 @@ const wrapText = s => {
 
 wrapText(".hello-text");
 wrapText(".i-am-text");
-wrapText('.details')
+wrapText(".details");
 
 const animateLetterEnter = letterEl => {
   anime.remove(letterEl);
@@ -23,7 +27,7 @@ const animateLetterEnter = letterEl => {
     scale: 1.3,
     rotateZ: -5,
     duration: 500,
-    easing: 'easeOutElastic(2.5, 0.3)',
+    easing: "easeOutElastic(2.5, 0.3)"
   });
 };
 
@@ -35,7 +39,7 @@ const animateLetterLeave = letterEl => {
     rotateZ: 0,
     duration: 500,
     opacity: 1,
-    easing: 'easeOutElastic',
+    easing: "easeOutElastic"
   });
 };
 
@@ -52,10 +56,33 @@ letters.forEach(letterEl => {
 });
 
 anime({
-    targets: ".square",
-    rotate: 90,
-    duration: 2000,
-    loop: true,
-    delay: anime.stagger(15),
-    scale: [1, 0.8, 1],
-})
+  targets: ".square",
+  rotate: 90,
+  duration: 2000,
+  loop: true,
+  delay: anime.stagger(15),
+  scale: [1, 0.8, 1]
+});
+
+function animateBars() {
+  let interval = Math.floor((Math.random()*6)+5);
+  // interval = Math.floor(Math.random()*2) == 0 ? interval : -interval;
+  const tl = anime.timeline()
+  tl.add({
+    targets: '.bar',
+    translateX: [0, anime.stagger([`${interval}%`, `${interval*0.4}%`], {from: 'center'})],
+    easing: "easeOutElastic(2, .5)",
+    delay: anime.stagger(60),
+    duration: 800,
+  })
+  .add({
+    targets: '.bar',
+    translateX: [anime.stagger([`${interval}%`, `${interval*0.4}%`], {from: 'center'}), 0],
+    easing: "easeOutElastic(2, .5)",
+    delay: anime.stagger(60, {direction: 'reverse'}),
+    duration: 800,
+  })
+  tl.complete = animateBars;
+}
+
+animateBars();
