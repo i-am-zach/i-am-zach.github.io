1c1609f0e6b3d64103e5f50858f8b77795d62ca3
Zach Lefkovitz <zachlefko@gmail.com>
2020-03-24 20:12:57 -0500

    Added skills section

--- animations.js
+++ animations.js
@@ -6,8 +6,6 @@ const secondary = getComputedStyle(document.body).getPropertyValue(
   "--secondary"
 );
 
-console.log(textPrimary);
-
 const wrapText = s => {
   const textWrapper = document.querySelector(s);
   textWrapper.innerHTML = textWrapper.textContent.replace(
@@ -74,7 +72,7 @@ anime({
   targets: shadowObj,
   loop: true,
   direction: "alternate",
-  prop1: 0.8,
+  prop1: 1.2,
   easing: "easeOutQuad",
   duration: 1000,
   update: () => {
@@ -88,25 +86,126 @@ anime({
   }
 });
 
-// function animateBars() {
-//   let interval = Math.floor((Math.random()*6)+5);
-// interval = Math.floor(Math.random()*2) == 0 ? interval : -interval;
-//   const tl = anime.timeline()
-//   tl.add({
-//     targets: '.bar',
-//     translateX: [0, anime.stagger([`${interval}%`, `${interval*0.4}%`], {from: 'center'})],
-//     easing: "easeOutElastic(2, .5)",
-//     delay: anime.stagger(60),
-//     duration: 800,
-//   })
-//   .add({
-//     targets: '.bar',
-//     translateX: [anime.stagger([`${interval}%`, `${interval*0.4}%`], {from: 'center'}), 0],
-//     easing: "easeOutElastic(2, .5)",
-//     delay: anime.stagger(60, {direction: 'reverse'}),
-//     duration: 800,
-//   })
-//   tl.complete = animateBars;
-// }
 
-// animateBars();
+//? ANIMATE SKILLS
+
+const skilltl = anime.timeline({autoplay: false})
+skilltl
+.add({
+  targets: ".skill-text",
+  opacity: [0, 1],
+  easing: "linear",
+  duration: 1000,
+})
+.add({
+  targets: '.skill',
+  translateX: function(el, i, l) {
+    if(el.className.includes('even')){
+      return [1000, 0]
+    } else {
+      return [-1000, 0]
+    }
+  },
+  easing: "easeOutElastic(.3, 3)",
+  delay: anime.stagger(500),
+})
+.add({
+  targets: '.skill .skill-name .skill-trail',
+  scale: [0, 1],
+  duration: 1000,
+})
+.add({
+  targets: ".skill .skill-icon",
+  keyframes: [
+    {scale: 0.75},
+    {scale: 1.5},
+    {scale: 1},
+  ],
+  duration: 500,
+  easing: 'easeOutQuad',
+})
+
+const skillsSection = document.querySelector("section.skills")
+
+const enterSkillIcon = (skillEl) => {
+  anime.remove(skillEl)
+  anime({
+    targets: [skillEl],
+    scale: 1.5,
+    duration: 500,
+  })
+}
+
+const leaveSkillIcon = (skillEl) => {
+  anime.remove(skillEl)
+  anime({
+    targets: skillEl,
+    scale: 1,
+    duration: 500,
+  })
+}
+
+const skillIcons = document.querySelectorAll('.skill-icon')
+skillIcons.forEach(skillEl => {
+  skillEl.addEventListener('mouseenter', () => {
+    enterSkillIcon(skillEl);
+  })
+  skillEl.addEventListener('mouseleave', () => {
+    leaveSkillIcon(skillEl);
+  })
+})
+
+
+// Animate About section
+const aboutAnimation  = anime({
+  autoplay: false,
+  targets: "#about",
+  opacity: {value: [0, 1], easing: 'linear', duration: 1200},
+  translateX: ['-100%', 0],
+  easing: "easeOutQuad",
+  duration: 800,
+})
+
+// Animate contact section
+const contactAnimation = anime({
+  targets: "section.contact .contact-wrapper",
+  autoplay: false,
+  opacity: {value: [0, 1], easing: 'linear', duration: 1200},
+  translateX: ['100%', 0],
+  easing: "easeOutQuad",
+  duration: 800,
+})
+
+
+function handleScroll() {
+  const scrollY = window.scrollY;
+  const skillsTop = document.querySelector("section.skills").clientHeight;
+  const contactTop = document.querySelector("section.contact").clientHeight;
+
+  if(scrollY > skillsTop) {
+    if(!skilltl.began) {
+      skilltl.play();
+    }
+  } else {
+    skilltl.reset();
+  }
+
+  if(scrollY > contactTop) {
+    if(!contactAnimation.began) {
+      contactAnimation.play();
+    }
+  } else {
+    contactAnimation.reset();
+  }
+
+  if(scrollY <= 5) {
+    aboutAnimation.reset();
+  } else {
+    if(!aboutAnimation.began) {
+      aboutAnimation.play();
+    }
+  }
+  
+}
+
+window.addEventListener("scroll", handleScroll)
