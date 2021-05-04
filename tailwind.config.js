fb10489be0183c2cb676d379a56152fe2ae6121a
i-am-zach <zachlefko@gmail.com>
2021-05-04 08:53:31 -0500

    Hugo/tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ["./static/**/*.css"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
