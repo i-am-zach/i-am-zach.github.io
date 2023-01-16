4aaf08ce044f5a9949306efbed69f8d37466c11f
Zach Lefkovitz <zachlefko@gmail.com>
2023-01-16 11:25:30 -0600

    Added markdown content and TOS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,19 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://i-am-zach.github.io',
-  integrations: [mdx(), sitemap(), tailwind()]
+  site: "https://i-am-zach.github.io",
+  markdown: {
+    rehypePlugins: [
+      "rehype-slug",
+      ["rehype-autolink-headings", { behavior: "append" }],
+      ["rehype-toc", { headings: ["h1", "h2"] }],
+    ],
+  },
+  integrations: [mdx(), sitemap(), tailwind()],
 });
